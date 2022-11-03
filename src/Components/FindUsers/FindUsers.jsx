b0e8d9fab4e76fea755b3f9aacd34348970c5b08
Yuriy <dementev.yuriy@list.ru>
2022-11-03 11:16:25 +0300

    Добавил пакет axios и реализовал вывод пользователей с сервера

--- src/Components/FindUsers/FindUsers.jsx
+++ src/Components/FindUsers/FindUsers.jsx
@@ -1,45 +1,16 @@
 import React from 'react';
 import classes from './FindUsers.module.css';
+import axios from "axios";
+import userFhoto from '../../assets/images/avatarDefault.png';
 
 function FindUsers(props) {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    name: 'Stela',
-                    avatar: 'https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=740&t=st=1665922271~exp=1665922871~hmac=d991083635ddce751d0ffca987adebaa59b751da06e7cc584828800a7ccef4e6',
-                    status: 'Dreams ...',
-                    location: {
-                        country: 'Russia',
-                        city: 'Volgograd',
-                    },
-                    followed: true,
-                },
-                {
-                    id: 2,
-                    name: 'Lera',
-                    avatar: 'https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=740&t=st=1665922271~exp=1665922871~hmac=d991083635ddce751d0ffca987adebaa59b751da06e7cc584828800a7ccef4e6',
-                    status: 'Dreams ...',
-                    location: {
-                        country: 'Russia',
-                        city: 'Moscow',
-                    },
-                    followed: false,
-                },
-                {
-                    id: 3,
-                    name: 'David',
-                    avatar: 'https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=740&t=st=1665922271~exp=1665922871~hmac=d991083635ddce751d0ffca987adebaa59b751da06e7cc584828800a7ccef4e6',
-                    status: 'Dreams ...',
-                    location: {
-                        country: 'Russia',
-                        city: 'Omsk',
-                    },
-                    followed: true,
-                },
-            ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(
+                response.data.items
+            )
+        });
     }
     return (
         <div>
@@ -47,7 +18,7 @@ function FindUsers(props) {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div className={classes.avatar}>
-                            <img src={u.avatar} alt=""/>
+                            <img src={u.photos.small != null ? u.photos.small: userFhoto}/>
                         </div>
                         <div>
                             {u.followed ? <button onClick={() => {
@@ -65,8 +36,8 @@ function FindUsers(props) {
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>u.location.country</div>
+                            <div>u.location.city</div>
                         </span>
                     </span>
                 </div>)
