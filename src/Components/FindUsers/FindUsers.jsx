42801f13c70743d10b41ed463e813ce3ac45c8d5
Yuriy <dementev.yuriy@list.ru>
2022-11-08 10:43:26 +0300

    Сделал контейнерную компоненту для FindUsers

--- src/Components/FindUsers/FindUsers.jsx
+++ src/Components/FindUsers/FindUsers.jsx
@@ -1,82 +1,64 @@
 import React from 'react';
 import classes from './FindUsers.module.css';
-import axios from "axios";
 import userPhoto from '../../assets/images/avatarDefault.png';
+import axios from "axios";
 
-class Users extends React.Component {
+function FindUsers(props) {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            });
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(
-                    response.data.items
-                )
-            });
-    }
+    let curP = props.currentPage;
+    let curPF = ((curP - 9) < 0) ? 0 : curP - 9;
+    let curPL = curP + 9;
+    let slicedPages = pages.slice(curPF, curPL);
 
-    render() {
+    return (
+        <div>
+            <div className={classes.containerSelects}>
+                {slicedPages.map(p => {
+                    return <div className={classes.containerSelect}>
+                        <div onClick={(e) => {
+                            props.onPageChanged(p)
+                        }} className={props.currentPage === p && classes.select}>{p}</div>
+                    </div>
+                })}
 
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        let curP = this.props.currentPage;
-        let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
-        let curPL = curP + 5;
-        let slicedPages = pages.slice(curPF, curPL);
-        return (
-            <div>
-                <div>
-                    {slicedPages.map(p => {
-                        return <span onClick={(e) => {
-                            this.onPageChanged(p)
-                        }} className={this.props.currentPage === p && classes.select}>{p}</span>
-                    })}
-
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
-                        <div className={classes.container}>
-                            <div className={classes.containerOne}>
-                                <div className={classes.avatar}>
-                                    <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
-                                </div>
-                                <div className={classes.btnFollowOrUnfollow}>
-                                    {u.followed ? <button className={classes.followBtn} onClick={() => {
-                                            this.props.follow(u.id)
-                                        }}>Follow</button> :
-                                        <button className={classes.unFollowBtn} onClick={() => {
-                                            this.props.unfollow(u.id)
-                                        }}>Unfollow</button>}
-
-                                </div>
+            </div>
+            {
+                props.users.map(u => <div key={u.id}>
+                    <div className={classes.container}>
+                        <div className={classes.containerOne}>
+                            <div className={classes.avatar}>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
                             </div>
-                            <div className={classes.containerTwo}>
-                                <div className={classes.fullname}>{u.name}</div>
-                                <div className={classes.status}>{u.status}This is status</div>
-                            </div>
-                            <div className={classes.containerThree}>
-                                <div className={classes.country}>u.location.country</div>
-                                <div className={classes.city}>u.location.city</div>
+                            <div className={classes.btnFollowOrUnfollow}>
+                                {u.followed ? <button className={classes.followBtn} onClick={() => {
+                                        props.follow(u.id)
+                                    }}>Follow</button> :
+                                    <button className={classes.unFollowBtn} onClick={() => {
+                                        props.unfollow(u.id)
+                                    }}>Unfollow</button>}
+
                             </div>
                         </div>
-                    </div>)
-                }
-            </div>
-        )
-    }
+                        <div className={classes.containerTwo}>
+                            <div className={classes.fullname}>{u.name}</div>
+                            <div className={classes.status}>{u.status}This is status</div>
+                        </div>
+                        <div className={classes.containerThree}>
+                            <div className={classes.country}>u.location.country</div>
+                            <div className={classes.city}>u.location.city</div>
+                        </div>
+                    </div>
+                </div>)
+            }
+        </div>
+    )
 }
 
-export default Users;
+
+export default FindUsers;
--- src/Components/FindUsers/FindUsersContainer.jsx
+++ src/Components/FindUsers/FindUsersContainer.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 import {connect} from "react-redux";
-import FindUsers from "./FindUsers";
 import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../Redux/findUsersReducer";
+import axios from "axios";
+import FindUsers from "./FindUsers";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(
+                    response.data.items
+                )
+            });
+    }
+
+    render() {
+
+        return (
+            <FindUsers
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -33,6 +70,6 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-const FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(FindUsers)
+const FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
 
 export default FindUsersContainer;
